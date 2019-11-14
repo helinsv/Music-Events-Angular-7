8a8229b65c0a827570a381b2ab514effd176e4cc
Helin Shevchuk <HSHEVCHU@ford.com>
2019-11-14 16:04:46 +0000

    Add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { PriceRangePipe } from './price-range.pipe';
-
+import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusiceventComponent,
     PriceRangePipe,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
