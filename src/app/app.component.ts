61f014dd9705250d2e72570773d109e58bf08b0c
Helin Shevchuk <HSHEVCHU@ford.com>
2019-11-14 15:06:51 +0000

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MusicEvent} from './MusicEvent';
+import { MusicEventService } from './music-event.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  events: MusicEvent[] = [{
-    name: 'Lowlands',
-    date: new Date('04/26/2019'),
-    price: 22,
-    description: 'Legendary Artists from all over worlds',
-    imageSrc: 'https://image.freepik.com/vrije-psd/moderne-banner-van-verfborstel-op-roze-achtergrond_24972-212.jpg'
-  }, {
-    name: 'Lolapalooza',
-    date: new Date('11/31/2019'),
-    price: 120,
-    description: 'Lolapalooza is the best festival in the world',
-    imageSrc: 'https://image.freepik.com/vrije-psd/decoratieve-gelukkige-de-groatachtergrond-van-het-holifestival_1017-17518.jpg'
-  }, {
-    name: 'Lowlands2',
-  date: new Date('04/26/2019'),
-  price: 86,
-  description: 'Legendary Artists from all over worlds',
-  imageSrc: 'https://image.freepik.com/vrije-psd/moderne-banner-van-verfborstel-op-roze-achtergrond_24972-212.jpg'
-}, {
-  name: 'Lolapalooza2',
-  date: new Date('11/31/2019'),
-  price: 55,
-  description: 'Lolapalooza is the best festival in the world',
-  imageSrc: 'https://image.freepik.com/vrije-psd/decoratieve-gelukkige-de-groatachtergrond-van-het-holifestival_1017-17518.jpg'
-}];
+export class AppComponent implements OnInit {
+
+  musicevents: MusicEvent[];
+
+  constructor(private musicEventService: MusicEventService) { }
+
+  ngOnInit(): void {
+    this.musicevents = this.musicEventService.GetMusicEvents();
+  }
 
 logName(value: string){
   console.log(value);
---
+++ src/app/music-event.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { MusicEvent} from './MusicEvent';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicEventService {
+  events: MusicEvent[] = [{
+    name: 'Lowlands',
+    date: new Date('04/26/2019'),
+    price: 22,
+    description: 'Legendary Artists from all over worlds',
+    imageSrc: 'https://image.freepik.com/vrije-psd/moderne-banner-van-verfborstel-op-roze-achtergrond_24972-212.jpg'
+  }, {
+    name: 'Lolapalooza',
+    date: new Date('11/31/2019'),
+    price: 120,
+    description: 'Lolapalooza is the best festival in the world',
+    imageSrc: 'https://image.freepik.com/vrije-psd/decoratieve-gelukkige-de-groatachtergrond-van-het-holifestival_1017-17518.jpg'
+  }, {
+    name: 'Lowlands2',
+  date: new Date('04/26/2019'),
+  price: 86,
+  description: 'Legendary Artists from all over worlds',
+  imageSrc: 'https://image.freepik.com/vrije-psd/moderne-banner-van-verfborstel-op-roze-achtergrond_24972-212.jpg'
+}, {
+  name: 'Lolapalooza2',
+  date: new Date('11/31/2019'),
+  price: 55,
+  description: 'Lolapalooza is the best festival in the world',
+  imageSrc: 'https://image.freepik.com/vrije-psd/decoratieve-gelukkige-de-groatachtergrond-van-het-holifestival_1017-17518.jpg'
+}];
+  constructor() { }
+
+GetMusicEvents(): MusicEvent[]{
+  return this.events;
+}
+}
